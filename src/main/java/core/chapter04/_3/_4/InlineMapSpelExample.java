e455e0257625ebbc453a2d0bdb40ed39f7e038b1
edfeff <396859442@qq.com>
2019-10-26 17:11:19 +0800

    spel map的解析和表示{key:value}

---
+++ src/main/java/core/chapter04/_3/_4/InlineMapSpelExample.java
@@ -0,0 +1,24 @@
+package core.chapter04._3._4;
+
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+import java.util.Map;
+
+/**
+ * map
+ * {key:value}
+ *
+ * @author wangpp
+ */
+public class InlineMapSpelExample {
+    public static void main(String[] args) {
+        ExpressionParser parser = new SpelExpressionParser();
+        Map<String, String> normalMap = (Map<String, String>) parser.parseExpression("{name:'wpp',address:'home'}").getValue();
+        System.out.println(normalMap);
+
+        Map<String, Map<String, String>> mapofMap
+                = (Map<String, Map<String, String>>) parser.parseExpression("{name:{first:'wpp',second:'pp'},address:{day:'10',hours:'9'}}").getValue();
+        System.out.println(mapofMap);
+    }
+}
